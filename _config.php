517de21eaffb4fa01dcb1a1df143cb78be2f9481
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-12-29 16:59:01 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://aniwatch-api-tau-flax.vercel.app"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://hianime-mapper-delta.vercel.app"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
